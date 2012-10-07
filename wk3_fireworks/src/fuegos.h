62153030c64caf653a13e0fd839f2b5d6b30f648
catacortazar <catacortazar@gmail.com>
2012-10-07 17:14:41 -0400

    assignment work in progress

---
+++ wk3_fireworks/src/fuegos.h
@@ -0,0 +1,13 @@
+//
+//  fuegos.h
+//  bounceOffTheWalls
+//
+//  Created by catalina cortazar on 10/7/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fuegos : NSObject
+
+@end
---
+++ wk3_fireworks/src/fuegos.m
@@ -0,0 +1,13 @@
+//
+//  fuegos.m
+//  bounceOffTheWalls
+//
+//  Created by catalina cortazar on 10/7/12.
+//
+//
+
+#import "fuegos.h"
+
+@implementation fuegos
+
+@end
